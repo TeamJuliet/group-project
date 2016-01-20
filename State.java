a1c2c06df8debab6c137a3015ebd193607a8d339
Artem Vasenin <vasart169@gmail.com>
2016-01-20 03:52:39 +0000

    Added an explanation

--- State.java
+++ State.java
@@ -1,5 +1,5 @@
 public class State {
   public State getCell(int x, int y);
-  public State isLegal(int x, int y, boolean direction);
+  public State isLegal(int x, int y, boolean direction); //direction: true for right, false for down
   public void makeMove(int x, int y, boolean direction);
 }
