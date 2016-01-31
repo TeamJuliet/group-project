d652da88b696b18af47209ef0b1bbdf103246ddd
BenNaccarato <benjamin@naccarato.plus.com>
2016-01-31 22:07:13 +0000

    works in progress
    added objective to design

--- src/uk/ac/cam/cl/intelligentgamedesigner/coregame/Design.java
+++ src/uk/ac/cam/cl/intelligentgamedesigner/coregame/Design.java
@@ -6,14 +6,19 @@ public class Design implements Serializable {
 	private int height, width;          // The dimensions of the board.
     private Cell[][] boardLayout;       // The layout - specifying the position of special candies and blocks
     private int numberOfMoves;          // The number of moves the player has available
+    private int objectiveTarget;		// The target needed to clear whatever game mode
+    	//(if high score, then = the required score)
+    	//(if jelly, then ignore this)
+    	//(if ingredients, then = the number of ingredients that need to be cleared)
     private GameMode gameMode;			// The game mode
     
-    public Design(){
+    public Design(){ //initialise with default values. specified elsewhere
     	height = 10;
     	width = 10;
     	boardLayout = new Cell[width][height];
     	numberOfMoves = 10;
     	gameMode = GameMode.HIGHSCORE;
+    	objectiveTarget = 1;
     }
     public void setSize(int width, int height){
     	this.width = width;
@@ -22,9 +27,10 @@ public class Design implements Serializable {
     public void setBoard(Cell[][] board){
     	boardLayout = board;
     }
-    public void setRules(GameMode gameMode, int numberOfMoves){
+    public void setRules(GameMode gameMode, int numberOfMoves, int objectiveTarget){
     	this.gameMode = gameMode;
     	this.numberOfMoves = numberOfMoves;
+    	this.objectiveTarget = objectiveTarget;
     }
     
     public int getWidth(){
