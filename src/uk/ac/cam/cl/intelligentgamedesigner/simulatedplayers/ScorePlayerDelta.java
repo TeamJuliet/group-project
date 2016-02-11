fb934cefbead1a3ce1151a3fb98ba4ee88d6fcd1
Artem Vasenin <vasart169@gmail.com>
2016-02-11 12:18:48 +0000

    Arranged simulated players in correct order.

--- src/uk/ac/cam/cl/intelligentgamedesigner/simulatedplayers/ScorePlayerDelta.java
+++ src/uk/ac/cam/cl/intelligentgamedesigner/simulatedplayers/ScorePlayerDelta.java
@@ -1,15 +1,21 @@
 package uk.ac.cam.cl.intelligentgamedesigner.simulatedplayers;
 
+import java.util.List;
+
 import uk.ac.cam.cl.intelligentgamedesigner.coregame.GameState;
 import uk.ac.cam.cl.intelligentgamedesigner.coregame.InvalidMoveException;
 import uk.ac.cam.cl.intelligentgamedesigner.coregame.Move;
+import uk.ac.cam.cl.intelligentgamedesigner.coregame.UnmoveableCandyGenerator;
 
-public class ScorePlayerDelta implements SimulatedPlayerBase {
-    GameState level;
+
+//Simple Evaluation: Metric = current score, Potential = best score after one move
+public class ScorePlayerDelta extends DepthPotentialPlayer {
     
-    public ScorePlayerDelta(GameState level){
+    public ScorePlayerDelta(GameState level, int numOfStatesAhead, int numOfStatesInPool) {
+        super(numOfStatesAhead, numOfStatesInPool);
         this.level = level;
     }
+    
     @Override
     public void solve() throws NoMovesFoundException {
         while (level.getMovesRemaining() > 0) {
@@ -27,16 +33,47 @@ public class ScorePlayerDelta implements SimulatedPlayerBase {
         }
     }
 
-    public static Move calculateBestMove(GameState currentState) throws NoMovesFoundException {
-        // Look through all possible moves and try to make special candies if
-        // possible
-        // Try to bring special candies together and combine them
-        // 
-        return null;
+    private void printInvalidMoveError(Move move) {
+        System.err.println("WARNING! DepthPotentialPlayerAlpha has suggested an invalidMove " + move + ".");
     }
 
-    private void printInvalidMoveError(Move move) {
-        System.err.println("WARNING! ScorePlayerDelta has suggested an invalidMove " + move + ".");
+    @Override
+    GameStateMetric getGameStateMetric(GameState gameState) {
+        return new GameStateMetric(gameState.getScore());
+    }
+
+    @Override
+    GameStatePotential getGameStatePotential(GameState gameState) {
+        //Return the highest increase in score of all possible matches
+        GameState original = new GameState(gameState, new UnmoveableCandyGenerator());
+        int highestIncrease = 0;
+        List<Move> moves = original.getValidMoves();
+        for(Move move: moves){
+            GameState tmp = original;
+            try{
+                tmp.makeMove(move);
+            } catch (InvalidMoveException e){
+                //Just eat the exception since we don't care if wrong move is suggested
+                System.err.println("Invalid move suggested in DepthPotentialPlayerAlpha evaluation");
+                continue;
+            }
+            
+            int increase = tmp.getScore() - original.getScore();
+            if(increase > highestIncrease) highestIncrease = increase;
+        }
+        return new GameStatePotential(highestIncrease);
+    }
+
+    @Override
+    GameStateCombinedMetric getCombinedMetric(GameStateMetric metric, GameStatePotential potential) {
+        //Value metric and potential equally for now and find arithmetic mean
+        return new GameStateCombinedMetric(metric, potential, (metric.score + potential.potential)/2);
+    }
+
+    @Override
+    List<Move> selectMoves(GameState gameState) {
+        //TODO: look more into filtering moves
+        return gameState.getValidMoves();
     }
 
 }
