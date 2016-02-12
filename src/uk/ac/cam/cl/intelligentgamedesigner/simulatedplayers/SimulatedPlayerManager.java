7f84f7a1ab245f5923510c5cd9407f4201509aff
Artem Vasenin <vasart169@gmail.com>
2016-02-12 16:43:20 +0000

    Added more functions to simulated players.

--- src/uk/ac/cam/cl/intelligentgamedesigner/simulatedplayers/SimulatedPlayerManager.java
+++ src/uk/ac/cam/cl/intelligentgamedesigner/simulatedplayers/SimulatedPlayerManager.java
@@ -5,26 +5,41 @@ import uk.ac.cam.cl.intelligentgamedesigner.coregame.Move;
 import uk.ac.cam.cl.intelligentgamedesigner.simulatedplayers.*;
 
 public class SimulatedPlayerManager {
-    public static Move calculateBestMove(GameState level, int ability) throws NoMovesFoundException{
-        SimulatedPlayerBase player = null;
+
+    private static SimulatedPlayerBase makeSimulatedPlayer(int ability) {
+        SimulatedPlayerBase player;
         switch (ability) {
-        case 2:
+        case 1:
             player = new ScorePlayerBeta();
             break;
-        case 3:
+        case 2:
             player = new ScorePlayerGamma();
             break;
-        case 4:
-            player = new DepthPotentialScorePlayer(4,4);
+        case 3:
+            player = new DepthPotentialScorePlayer(4, 4);
             break;
         default:
             player = new ScorePlayerAlpha();
             break;
         }
-        
+        return player;
+    }
+
+    public static Move calculateBestMove(GameState level, int ability) throws NoMovesFoundException {
+        SimulatedPlayerBase player = makeSimulatedPlayer(ability);
         return player.calculateBestMove(level);
-        
-        //TODO: make function with possible difficulties, make function that uses simulatedPlayers
-        
+
+        // TODO: make function with possible difficulties, make function that
+        // uses simulatedPlayers
+
+    }
+
+    public static int getMaxAbilityLevel() {
+        return 3;
+    }
+
+    public static void solve(GameState level, int ability) throws NoMovesFoundException {
+        SimulatedPlayerBase player = makeSimulatedPlayer(ability);
+        player.solve(level);
     }
 }
