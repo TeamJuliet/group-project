9f59e6c994311c51440d8657516480bcf933e5e5
ciaran16 <ciaranlawlor16@gmail.com>
2016-02-25 12:07:45 +0000

    Removing parameters, should give a small speedup

--- src/uk/ac/cam/cl/intelligentgamedesigner/leveldesigner/ArrayLevelRepresentationScore.java
+++ src/uk/ac/cam/cl/intelligentgamedesigner/leveldesigner/ArrayLevelRepresentationScore.java
@@ -7,35 +7,13 @@ import uk.ac.cam.cl.intelligentgamedesigner.coregame.GameMode;
 
 public class ArrayLevelRepresentationScore extends ArrayLevelRepresentation {
 
-    /*
-        Parameters list:
-        ----------------
-        [0] - number of moves available
-        [1] - score to reach
-
-        Board cell types:
-        -----------------
-        0 => Empty
-        1 => Unusable
-        2 => Icing
-        3 => Liquorice
-    */
-
     public ArrayLevelRepresentationScore(Random random, int numberOfCandyColours) {
         super(random, numberOfCandyColours);
-
-        // Score to reach is initialised in the range: 100-50000
-        // For the score, we want it to be a multiple of 10, so we multiply this parameter value by 10 (see below)
-        this.parameters.add(new Parameter(random, 100, 50000));
     }
     
     @Override
     public Design getDesign() {
     	Design design = super.getDesign();
-    
-    	design.setGameMode(GameMode.HIGHSCORE);
-        // This ensures the score is a multiple of 10 (like in Candy Crush)
-        design.setObjectiveTarget(parameters.get(1).getValue() * 10);
     	
         return design;
     }
