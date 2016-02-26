26edfd85dadb75769510f2d2509a0fcce7a8f603
Dimitrios <cam.dimitrislos@gmail.com>
2016-02-26 00:42:17 +0000

    Minor Changes and added documentation.

--- src/uk/ac/cam/cl/intelligentgamedesigner/coregame/CandyGenerator.java
+++ src/uk/ac/cam/cl/intelligentgamedesigner/coregame/CandyGenerator.java
@@ -4,39 +4,66 @@ import java.io.Serializable;
 
 /**
  * 
- * Abstract class that is used to generate new candies for the positions to be filled.
- * Note: that the function setDesignAndGameProgress has to been called before being called.
+ * Abstract class that is used to generate new candies for the positions to be
+ * filled. Note: that the function setDesignAndGameProgress has to been called
+ * before being called.
  *
  */
 public abstract class CandyGenerator implements Serializable {
-    
+
     // The level design used to generate the game that is being played.
-    protected Design design;
-    
+    protected Design            design;
+
     // The game state progress for the current game.
     protected GameStateProgress gameStateProgress;
-    
-    protected int previousNumberOfIngredientsRemaining;
-    
-    // Remaining ingredients that have to drop.
-    protected int ingredientsToDrop;
 
-    // Some CandyGenerators do not need the GameState to throw the next candy.
-    public CandyGenerator () {
-    	this.ingredientsToDrop = 0;
+    protected int               previousNumberOfIngredientsRemaining;
+
+    // Remaining ingredients that have to drop.
+    protected int               ingredientsToDrop;
+
+    /**
+     * The default candy generator constructor.
+     */
+    public CandyGenerator() {
+        this.ingredientsToDrop = 0;
     }
-    
-    public void setDesignAndGameProgress (Design design, GameStateProgress gameStateProgress) {
-    	this.design = design;
+
+    /**
+     * Function that sets the design and game state progress of the game state.
+     * Note: this should be done in the constructor of GameState.
+     * 
+     * @param design
+     *            The design used for generating the level. (This could be null
+     *            indicating that none was used in creating the GameState.
+     * @param gameStateProgress
+     *            The GameStateProgress, so that ingredients can be dropped
+     *            during the course of the game. (This should not be null)
+     */
+    public void setDesignAndGameProgress(Design design, GameStateProgress gameStateProgress) {
+        this.design = design;
         this.gameStateProgress = gameStateProgress;
-        if (this.design == null) return;
-        if (this.gameStateProgress == null) 
-        	System.err.println("Candy Generator Error: Game state progress is null");
+
+        // It is ok for the design to be null, since one might not have been
+        // used for creating a GameState.
+        if (this.design == null)
+            return;
+
+        if (this.gameStateProgress == null)
+            System.err.println("Candy Generator Error: Game state progress is null");
+
         this.previousNumberOfIngredientsRemaining = gameStateProgress.getIngredientsRemaining();
 
         this.ingredientsToDrop = gameStateProgress.getIngredientsRemaining() - 1;
     }
-    
+
+    /**
+     * The main function of the CandyGenerator that actually returns a candy
+     * given the x-coordinate at which the cell is located at.
+     * 
+     * @param x The x-coordinate of the cell.
+     * @return The candy generated.
+     */
     public abstract Candy generateCandy(int x);
 
     public boolean shouldGenerateIngredient() {
