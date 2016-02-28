5fa54f4531611dc2cdb1cd9beff84449284d3370
BenNaccarato <benjamin@naccarato.plus.com>
2016-02-28 00:28:22 +0000

    Analyse level implemented

--- src/uk/ac/cam/cl/intelligentgamedesigner/userinterface/Analyser.java
+++ src/uk/ac/cam/cl/intelligentgamedesigner/userinterface/Analyser.java
@@ -2,37 +2,82 @@ package uk.ac.cam.cl.intelligentgamedesigner.userinterface;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.WindowConstants;
 
 import uk.ac.cam.cl.intelligentgamedesigner.coregame.Design;
+import uk.ac.cam.cl.intelligentgamedesigner.leveldesigner.LevelDesignerManager;
 
-public class Analyser extends JPanel{
+public class Analyser extends JPanel implements ActionListener,PropertyChangeListener{
+	int ability;
+	Design design;
+	JLabel info;
+	JButton start;
+	JLabel progress;
+	JLabel result;
 
 	public Analyser(Design design, int ability){
 		super();
+		this.design = design;
+		this.ability = ability;
 		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
 		
-		JLabel text = new JLabel("Analysing...");
-		add(text);
-		String theText = "The Simulated Player with ability level " + ability + " finished with a pass rate of " + analyse(design) + "%";
-		text.setText("Done!");
-		//JTable table = new JTable(2,2);
-		//fillTable(table);
-		//add(table);
-		JLabel value = new JLabel(theText);
-		add(value);
+		info = new JLabel("Simulated Player Ability Level: " + ability );
+		start = new JButton("Begin Analysis");
+		start.addActionListener(this);
+		start.setActionCommand("start");
+		start.setEnabled(true);
+		
+		add(info);
+		add(start);
+		
+		progress = new JLabel("Progress: Not Started");
+		result = new JLabel("Pass Rate: N/A");
+		
+		add(progress);
+		add(result);
+		
 	}
-	
-	private int analyse(Design design){
-		double passrate = 1;
-		return (int)(passrate*100);
+	public void startAnalysis(){
+		start.setEnabled(false);
+		progress.setText("Progress: Analysing...");
+
+		AnalysisThread worker = new AnalysisThread(ability,design);
+		worker.addPropertyChangeListener(this);
+	    worker.execute();	
 	}
-	
-//	private void fillTable(JTable table){
-//		
-//	}
+	public void endAnalysis(double passrate){
+		//trim to 1dp.
+		passrate = (double)((int)(passrate * 1000))/10;
+		String theText = "Pass Rate: " + passrate + "%";
+		
+		progress.setText("Progress: Done!");
+		result.setText(theText);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch(e.getActionCommand()){
+		case "start":
+			startAnalysis();
+			break;
+		}
+	}
+	@Override
+	public void propertyChange(PropertyChangeEvent e) {
+		switch(e.getPropertyName()){
+		case "done":
+			endAnalysis((double)e.getNewValue());
+			break;
+		}
+	}
 }
